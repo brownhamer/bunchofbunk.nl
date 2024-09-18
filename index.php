bea8e65fb0500be38174c9c05f5694fdd2bfbf56
Hendrik <brownhamer@gmail.com>
2024-09-18 14:56:38 +0200

    Switched to only left sidebar

--- index.php
+++ index.php
@@ -27,11 +27,9 @@ startHtmlPage($PATH_TO_ROOT, $bob_title, $bob_line);
     &middot; <a href="<?php echo $bob_url . "?page=pleasedtomeetyou"; ?>">Pleased to Meet You</a><br />
     &middot; <a href="<?php echo $bob_url . "?page=difficult2nd"; ?>">Difficult 2nd</a><br />
     &middot; <a href="<?php echo $bob_url . "?page=fearless"; ?>">Fearless</a><br />
-</div>
-<div id="content">
-Content
-</div>
-<div id="socials">
+    <br />
+    <hr />
+    <br />
     <a href="https://open.spotify.com/artist/5hRck8ylzUPLXWXrcHKJ2D"><img alt="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://spotify.com/&size=16" src="images/spotify.png" /></a>
     <a href="https://www.reverbnation.com/bunchofbunk"><img alt="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://reverbnation.com/&size=16" src="images/reverbnation.png" /></a>
     <a href="https://soundcloud.com/bunch-of-bunk"><img alt="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://soundcloud.com/&size=16" src="images/soundcloud.png" /></a>
@@ -39,12 +37,14 @@ Content
     <a href="https://www.facebook.com/pages/Bunch-of-Bunk/137974419583188"><img alt="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://facebook.com/&size=16" src="images/facebook.png" /></a>
     <a href="https://www.instagram.com/bunchofbunk/"><img alt="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://instagram.com/&size=16" src="images/instagram.png" /></a>
     <a href="https://www.threads.net/@bunchofbunk"><img alt="https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://threads.net/&size=16" src="images/threads.png" /></a>
-    &middot;
     <a href="mailto:band@bunchofbunk.nl"><img alt="mail" src="images/mail.png" /></a><br />
     <br />
     <hr />
     <br />
     <a href="https://www.muzikantenbank.net/">Muzikantenbank.NET</a> voor als je muzikanten of een bandje zoekt.
+</div>
+<div id="content">
+Content
 </div>
 </div>
 
